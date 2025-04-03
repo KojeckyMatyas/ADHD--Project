4edc8f4d40bbf7ad925c9565592b06a8e4d814db
KojeckyMatyas <matyaskojecky@gmail.com>
2025-04-03 17:00:38 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function checkVisibility() {
-  const wrapper = document.querySelector('.fade-in-wrapper'); // Vybereme wrapper pro celý obsah
-  const rect = wrapper.getBoundingClientRect(); // Získáme pozici wrapperu vůči oknu
+  const wrapper = document.querySelector('.fade-in-wrapper'); 
+  const rect = wrapper.getBoundingClientRect(); 
 
-  // Pokud je celý wrapper na obrazovce, přidáme třídu "visible"
+  
   if (rect.top <= window.innerHeight && rect.bottom >= 0) {
     wrapper.classList.add('visible');
   } else {
-    wrapper.classList.remove('visible'); // Pokud není viditelný, třídu odebereme
+    wrapper.classList.remove('visible'); 
   }
 }
 
-// Zavoláme funkci při každém scrollování
+
 window.addEventListener('scroll', checkVisibility);
 
-// Zavoláme funkci ihned po načtení stránky, aby animace začala hned, pokud je wrapper již viditelný
 document.addEventListener('DOMContentLoaded', checkVisibility);
